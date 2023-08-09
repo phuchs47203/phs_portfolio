7f0d085c98332e5c1069c9518f0cbdc158032f38
phuchs47203 <21522479@gm.uit.edu.vn>
2023-08-09 11:03:09 +0700

    edit url 33

--- src/service/CommentUserService.js
+++ src/service/CommentUserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const COMMENT_API_BASE_URL = "http://192.168.1.9:8080/api/v1/comment";
+const COMMENT_API_BASE_URL = "http://localhost:8080/api/v1/comment";
 
 
 class CommentUserService {
