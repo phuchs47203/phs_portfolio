35bf4e48ccb63310b23873f24538dd18f75fc412
phuchs47203 <21522479@gm.uit.edu.vn>
2023-10-20 21:57:10 +0700

    add background transparent

--- src/components/OneSkill/OneSkill.jsx
+++ src/components/OneSkill/OneSkill.jsx
@@ -3,13 +3,18 @@ import './oneskill.css';
 import { useState } from 'react';
 import { RiCloseLine } from 'react-icons/ri';
 import { VscEyeClosed } from 'react-icons/vsc';
+
+
 const OneSkill = ({ imgurl, content }) => {
     const [toggleMenu, settoggleMenu] = useState(false);
     return (
         <div className='pflo_oneskill'>
             {toggleMenu
-                ? <VscEyeClosed onClick={() => settoggleMenu(false)} />
+                ? <div className='close-icon'> <VscEyeClosed onClick={() => settoggleMenu(false)} /></div>
                 : <img src={imgurl} alt="logoSkill" onClick={() => settoggleMenu(true)} />
+            }
+            {
+                toggleMenu && <div className='background_transparent ' ></div>
             }
             {toggleMenu && (
                 <div className='pflo_oneskill-content-parent'>
